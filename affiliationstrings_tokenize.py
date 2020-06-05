6dffa19e8d8fa74e748be94bf160c4ad7a156750
Markus Reiter-Haas <iseratho@gmail.com>
2020-06-05 16:38:12 +0200

    WIP Entity Primitives, Currently Reshaping and Eval

--- affiliationstrings_tokenize.py
+++ affiliationstrings_tokenize.py
@@ -7,9 +7,15 @@ with open("./data/affiliationstrings/affiliationstrings_ids.csv", "r") as infile
         parts = line.split(",")
         id = parts[0].strip("\"")
         sentence = " ".join(parts[1:]).strip("\"")[:-2]
-        print(f"{id} - {sentence}")
+        # print(f"{id} - {sentence}")
         for token, count in Counter(sentence.split(" ")).most_common():
             if not token:
                 continue
-            print(f"{token} - {count}")
+            # print(f"{token} - {count}")
             outfile.write(f"{id},{token},{count}\n")
+
+with open("./data/affiliationstrings/affiliationstrings_mapping.csv", "r") as infile, \
+        open("./data/affiliationstrings/affiliationstrings_mapping_fixed.csv", "w") as outfile:
+    for line in infile.readlines():
+        fixed = line.replace("\"", "")
+        outfile.write(fixed)
